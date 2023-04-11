e691ba05ba32169cee1adfcf88ec2d25651cb827
yesaroun <akor1@naver.com>
2023-04-11 14:53:47 +0900

    feat: user 모델 생성

--- docker-server/server_dev/users/models.py
+++ docker-server/server_dev/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-hi
+
+class User(AbstractUser):
+    """
+    사용자를 나타내는 모델
+    """
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
