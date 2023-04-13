87527dfa33c8632952826149ef832838b08bd4e7
yesaroun <akor1@naver.com>
2023-04-13 13:37:45 +0900

    feat: translated variable serializer 생성

---
+++ docker-server/server_dev/variable_translation/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import TranslatedVariables
+
+
+class TranslatedVariablesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TranslatedVariables
+        fields = (
+            "korean_word",
+            "translated_variable",
+        )
