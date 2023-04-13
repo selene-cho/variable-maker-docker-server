14198a61c34d90e5eb3e0de139792bc189d49827
yesaroun <akor1@naver.com>
2023-04-13 13:32:10 +0900

    feat: variable translation 모델 생성

--- docker-server/server_dev/variable_translation/models.py
+++ docker-server/server_dev/variable_translation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class TranslatedVariables(models.Model):
+    """
+    번역된 단어들을 저장하는 모델
+    """
+
+    korean_word: str = models.CharField(
+        max_length=50,
+        help_text="한글로 입력한 단어",
+    )
+    translated_variable: str = models.CharField(
+        max_length=50,
+        help_text="번역된 결과 단어",
+    )
+
+    class Meta:
+        managed: bool = True
