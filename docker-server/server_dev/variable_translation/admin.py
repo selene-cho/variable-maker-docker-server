b6ab0530f9c76ff1e6a4b29e893781a8f8eeb31b
yesaroun <akor1@naver.com>
2023-04-18 13:07:25 +0900

    feat: variable tranlation admin 세팅

--- docker-server/server_dev/variable_translation/admin.py
+++ docker-server/server_dev/variable_translation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import TranslatedVariables
 
-# Register your models here.
+
+@admin.register(TranslatedVariables)
+class TVAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "korean_word",
+        "translated_variable",
+    )
+    list_display_links = (
+        "id",
+        "korean_word",
+        "translated_variable",
+    )
