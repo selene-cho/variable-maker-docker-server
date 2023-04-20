389c556df41ffef9bb1240845cae9c7c4a8d1f4a
yesaroun <akor1@naver.com>
2023-04-20 11:39:58 +0900

    feat: 축약 단어 모델 생성

--- docker-server/server_dev/variable_abbreviation/models.py
+++ docker-server/server_dev/variable_abbreviation/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 
 
-# class AbbreviatedVariables(models.Model):
-#     """
-#     축약된 단어들을 저장하는 모델
-#     """
-#
-#     answer: str = models.CharField(
-#         max_length=50,
-#         unique=True,
-#         help_text="축약하려는 단어",
-#     )
-#     abbreviated_variable: str = models.ForeignKey
+class Variable(models.Model):
+    """
+    축약할 단어들을 저장하는 모델
+    """
+
+    variable: str = models.CharField(
+        max_length=50,
+        unique=True,
+        help_text="축약하려는 단어",
+    )
+
+
+class AbbreviatedVariable(models.Model):
+    """
+    축약될 단어들을 저장하는 모델
+    """
+
+    variable: str = models.ForeignKey(
+        Variable,
+        on_delete=models.CASCADE,
+        related_name="abbreviatedVariables",
+    )
+    abbreviated_variable: str = models.CharField(
+        max_length=50,
+        help_text="축약된 단어",
+    )
