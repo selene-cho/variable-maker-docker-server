02dccc5aa03d5446ac7bfcf40a7e531dbc4c5155
yesaroun <akor1@naver.com>
2023-05-02 15:56:24 +0900

    https

--- docker-server/server_dev/server_dev/settings.py
+++ docker-server/server_dev/server_dev/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://223.130.128.91",
+    "https://223.130.128.91",
     "http://variablemaker.s3-website.ap-northeast-2.amazonaws.com",
     "https://variablemaker.kr",
 ]
